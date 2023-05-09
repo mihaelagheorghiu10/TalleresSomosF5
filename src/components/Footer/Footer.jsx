20816041add5b2aeb61fedbd2ef1aa0af61e1105
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-09 16:24:08 +0200

    added styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import logo from '../../assets/logoSomosF5.png'
 import {
   AiFillTwitterCircle,
   AiFillInstagram,
+  AiFillLinkedin,
 } from '../../../node_modules/react-icons/ai'
 // import { Link } from 'react-router-dom'
 
@@ -13,10 +14,40 @@ export default function Footer() {
       <div className={style.footerPageConteiner}>
         <div className={style.divLogo}>
           <img className={style.imgLogo} src={logo} alt="logoSomosF5.png"></img>
-          {/* </div> */}
           <p>Fundacion Somos F5</p>
           <p>Avinguda Bogatell, 82 Bajos</p>
           <p>Barcelona, 08005</p>
+          <p>info@somosf5.org</p>
+        </div>
+
+        <div className={style.footerLinksBox}>
+          <div className={style.linksItem}>
+            <h3>Apuntate</h3>
+            <p>Talleres</p>
+            <p>Formaciones</p>
+          </div>
+          <div className={style.linksItem}>
+            <h3>Partners</h3>
+            <p>Colabora</p>
+            <p>Contrata talento</p>
+          </div>
+          <div className={style.linksItem}>
+            <h3>La Fundacion</h3>
+            <p>Sobre F5</p>
+            <p>Contactanos</p>
+          </div>
+        </div>
+
+        <div className={style.socialMediaLogos}>
+          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+            <AiFillInstagram />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+            <AiFillTwitterCircle />
+          </a>
+          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+            <AiFillLinkedin />
+          </a>
         </div>
 
         {/* <Link className={style.link} to={'/register'}>
@@ -27,16 +58,21 @@ export default function Footer() {
         </Link>
         <p>Ayuda</p> */}
       </div>
+
       <div className={style.footerB}>
-        <p>Síguenos</p>
-        <div className={style.socialMediaLogos}>
+        <h4>SOMOSF5@TODOSLOSDERECHOSRESERVADOS</h4>
+        <h4>Politica de privacidad</h4>
+        <h4>Politica de cookies</h4>
+        {/* </div> */}
+
+        {/* <div className={style.socialMediaLogos}>
           <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
             <AiFillTwitterCircle />
           </a>
           <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
             <AiFillInstagram />
           </a>
-        </div>
+        </div> */}
       </div>
     </div>
   )
