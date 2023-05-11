e4fa3572e973eb4beb470ca96a9862297d365995
ifadoua2 <f647379@gmail.com>
2023-05-11 14:01:39 +0200

    create component Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import logo from '../../assets/logoSomosF5.png'; 
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logoSomosF5.png';
 import style from './Navbar.module.css';
 
 export default function Navbar() {
-
   return (
-
     <div className={style.NavbarContainer}>
       <div className={style.divLogo}>
-        <img className={style.imgLogo} src={logo} alt="logoSomosF5"/>
-      </div>
+        <img className={style.imgLogo} src={logo} alt="logoSomosF5" />
     </div>
-   
 
-   
-  )
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/itinerarios">Itinerarios</Link>
+          </li>
+          <li>
+            <Link to="/empresas">Empresas</Link>
+          </li>
+          <li>
+            <Link to="/partners">Partners</Link>
+          </li>
+          <li>
+            <Link to="/sobre-f5">Sobre F5</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
-
