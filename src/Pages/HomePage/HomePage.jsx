1e985c0a4fbe76d129e476b3a0640550a6e2c769
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-15 17:45:24 +0200

    added commit by homePage

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -6,8 +6,18 @@ function HomePage() {
     <div className={style.HomePageConteiner}>
       <div className={style.text1HomePage}>
         <h1 className={style.title1}>Aqui</h1>
-        <h1 className={style.title2}>empieza</h1>
+        <h1 className={style.title2}>
+          empieza el viaje hacia tu talento digital
+        </h1>
+        <h1 className={style.title1}>#Rompemosloscodigos</h1>
       </div>
+      <div className={style.eventCardsHomePage}>
+        <div className={style.eventCard1}>Talleres HTML y CSS</div>
+        <div className={style.eventCard2}>Talleres React</div>
+        <div className={style.eventCard3}>Talleres Java Script</div>
+        <div className={style.eventCard4}>Talleres Metaverso</div>
+      </div>
+      <div className={style.text2HomePage}></div>
     </div>
   )
 }
