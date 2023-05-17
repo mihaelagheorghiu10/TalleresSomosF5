1220657a27c1eb533f13d4137a6c6da89f21f8c2
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-17 12:57:55 +0200

    commit homePage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import HomePage from './Pages/HomePage/HomePage'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
