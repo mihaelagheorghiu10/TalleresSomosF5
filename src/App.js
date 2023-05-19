7b003e4b02a9d42239e67800eacaf316f7d668b3
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-19 03:55:19 +0200

    add commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App ()  {
+function App() {
   return (
+    // <Router>
     <div className="App">
       <Navbar />
+      {/* <Routes></Routes> */}
     </div>
+    // </Router>
   )
 }
 
