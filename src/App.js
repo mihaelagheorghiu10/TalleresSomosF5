1e8b47933dbfc3b0be77110ab72130bf2c4dd410
vanessa.cp <vanehom@hotmail.com>
2023-05-19 10:31:21 +0200

    cardsCategory

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css'
-
+import CategoryCards from './components/CategoryCards/CategoryCards';
+import beginnerLevel from './assets/images/beginnerLevel.png';
+ 
 function App() {
   return (
     <div className="App">
       <h1>"empieza el viaje hacia tu talento digital"</h1>
+      <CategoryCards beginnerLevel={beginnerLevel} src = "img" title = "hola" description = "somos f5" />
+
     </div>
   )
 }
