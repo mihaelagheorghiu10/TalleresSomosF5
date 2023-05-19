73735da01c64342bb0b5bf371d41c712523ea1d7
ifadoua2 <f647379@gmail.com>
2023-05-19 10:31:38 +0200

    added menu embr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App ()  {
