e869c66145c8c7b32098ab720beee4b686c3325f
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-19 10:59:32 +0200

    create route in app.jx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
-import HomePage from './pages/HomePage/HomePage'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import HomePage from './page/HomePage/HomePage'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/homePage" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Navbar /> */}
     </div>
   )
 }
