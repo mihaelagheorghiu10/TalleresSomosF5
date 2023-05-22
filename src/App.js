8c42c31b030bdec5244e449d65c34ce909f07152
mihaelagheorghiu10 <118915363+mihaelagheorghiu10@users.noreply.github.com>
2023-05-22 11:26:20 +0200

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import './App.css'
-import Footer from './components/Footer/Footer'
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import CategoryCards from './components/CategoryCards/CategoryCards'
-import beginnerLevel from './assets/images/beginnerLevel.png'
-
-import Navbar from './components/Navbar/Navbar'
 import HomePage from './page/HomePage/HomePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    // <Router>
     <div className="App">
-      {/* <h1>"empieza el viaje hacia tu talento digital"</h1> */}
-      <Footer />
-      <h1>"empieza el viaje hacia tu talento digital"</h1>
-      <CategoryCards
+      {/* <CategoryCards
         beginnerLevel={beginnerLevel}
         src="img"
         title="hola"
         description="somos f5"
-      />
+      /> */}
 
       <BrowserRouter>
         <Routes>
@@ -28,9 +18,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
-      {/* <Navbar /> */}
     </div>
-    // </Router>
   )
 }
 
