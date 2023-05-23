eff2f58ad7e6d699e71ee954e72f4ada5304bf99
vanessa.cp <vanehom@hotmail.com>
2023-05-23 11:38:21 +0200

    corregir

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import HomePage from './page/HomePage/HomePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+
 function App() {
   return (
     <div className="App">
