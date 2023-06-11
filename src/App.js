f69b25dd7badaadfe8312fe5fcadffc5ebb9eb08
ifadoua2 <f647379@gmail.com>
2023-06-11 14:44:11 +0200

    commit to dev

--- src/App.js
+++ src/App.js
@@ -3,18 +3,14 @@ import HomePage from './page/HomePage/HomePage'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Form from './page/Form/Form'
-import  Talleres  from './page/talleres/Talleres'
+import AdminPage from './page/AdminPage/AdminPage'
+import Talleres from './page/talleres/Talleres'
 import EventDetail from './page/EventDetail/EventDetail'
-
+import RegistrationForm from './page/registrationForm/RegistrationForm'
 
 function App() {
   return (
     <div className="App">
-
-
-
-
-
       <BrowserRouter>
         <Routes>
           <Route path="/homePage" element={<HomePage />} />
@@ -22,10 +18,11 @@ function App() {
           <Route path="/EventDetail/:level" element={<EventDetail />} />
           <Route path="/" element={<EventDetail />} />
           <Route path="/form" element={<Form />} />
-          <Route path="/talleres/:level"element={<Talleres/>}/>
+          <Route path="/adminPage" element={<AdminPage />} />
+          <Route path="/talleres/:level" element={<Talleres />} />
+          <Route path="/registrationForm" element={<RegistrationForm/>}/>
         </Routes>
       </BrowserRouter>
-
     </div>
   )
 }
